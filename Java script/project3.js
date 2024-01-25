45b7b3329c5c486cc51786dc45293c40505e7df9
Neerajprograms <neerajshikari303@gmail.com>
2024-01-26 03:16:09 +0530

    Newchanges

--- Java script/project3.js
+++ Java script/project3.js
@@ -4,6 +4,7 @@ const books = [{
       "title": "Unlocking Android",
       "isbn": "1933988673",
       "pageCount": 416,
+      "video":"",
       "publishedDate": { "$date": "2009-04-01T00:00:00.000-0700" },
       "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/ableson.jpg",
       "shortDescription": "Unlocking Android: A Developer's Guide provides concise, hands-on instruction for the Android operating system and development tools. This book teaches important architectural concepts in a straightforward writing style and builds on this with practical and useful examples throughout.",
